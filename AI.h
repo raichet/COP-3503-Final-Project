5d39b9ec9d8c452ba2851b70a22642bb52038531
kriswawrzyniak <kWawrzyniak@ufl.edu>
2017-04-15 11:23:03 -0400

    Create AI.h
    Began creating the AI class. We can implement two different algorithms for the AI logic. One would just randomly select spaces until all ships are hit. The other would randomly select spaces until a ship is hit and then attack every space around the hit space. That algorithm is described here: http://www.datagenetics.com/blog/december32011/

---
+++ AI.h
@@ -0,0 +1,31 @@
+#ifndef AI_H
+#define AI_H
+
+#include <string>
+
+using namespace std;
+
+class AI {
+
+	string difficulty; //Either 'Easy' or 'Hard'
+
+	int AIBoard[10][10];
+
+	int AIShips[] = { 5,4,3,2,2 };
+
+	bool winning;
+
+public:
+	AI(string difficulty);
+
+	void resetBoard();
+
+	void modifyShips(int x, int y, string orientation, int shipType);
+
+	bool isWin();
+
+	void attack(string difficulty); // Attacks the player using specified algorithm
+                                  // 'Easy' describes randomly selecting every space; 'Hard' describes selecting spaces at random and when a space is hit, intuitively selecting spaces around the hit space
+};
+
+#endif
