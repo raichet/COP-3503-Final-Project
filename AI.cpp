fe5c4393524e208c119686708901f85769268d9d
kriswawrzyniak <kWawrzyniak@ufl.edu>
2017-04-15 20:24:40 -0400

    started easy attack method
    need to finish easy attack method, hard attack method, and modify ships method

--- AI.cpp
+++ AI.cpp
@@ -64,15 +64,23 @@ int AI::updateBoard(int loc1, int loc2, int result){
 	AIBoard[loc1][loc2] = result;
 }
 
-void AI::attack(string difficulty){
-	
-	switch (difficulty){
-		
-		case easy: 
-			//Randomly generate hits without repeating
-		case hard: 
-			//Implement AI algorithm to attack here
-
+int AI::attackEasy(){
+	int attack = 0; 
+	int x;
+	int y; 
+	while (attack == 0){
+		attack = rand() % 10 //generate a number between 0 and 99 
+		//check to make sure that the number hasnt already been chosen 
+		if (/*attack is not contained in the vector "attackedSpaces"*/){
+			return attack; 
+		}
+		else 
+			attack = 0; // retry random generation 
 	}
+	
 
+}
+
+int AI::attackHard(){
+	//TODO - algorithm implementation 
 }
